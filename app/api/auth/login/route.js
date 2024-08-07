3f2eca9c0a120dd2044dc354f3ceb10b279defbe
jaypeter19 <joaopedrofava19@gmail.com>
2024-08-07 15:05:06 -0300

    route js modified

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,6 +1,6 @@
 import nextConnect from 'next-connect';
 import session from 'express-session';
-import passport from 'passport';
+import passport from '@/lib/passport'; // certifique-se que a importação está correta
 import connectMongoDB from "@/libs/mongodb";
 import MongoStore from 'connect-mongo';
 
@@ -26,13 +26,22 @@ handler.use(
 handler.use(passport.initialize());
 handler.use(passport.session());
 
-handler.post('api/auth/login', (req, res, next) => {
-  
+handler.post('/api/auth/login', (req, res, next) => { // Corrigido a rota
+    console.log('Recebendo uma requisição para login'); // Log para depuração
     passport.authenticate('local', (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.status(401).json({ message: info.message });
+    if (err) {
+      console.error('Erro na autenticação', err); // Log para depuração
+      return next(err);
+    }
+    if (!user) {
+      console.warn('Usuário não encontrado', info.message); // Log para depuração
+      return res.status(401).json({ message: info.message });
+    }
     req.logIn(user, (err) => {
-      if (err) return next(err);
+      if (err) {
+        console.error('Erro ao logar usuário', err); // Log para depuração
+        return next(err);
+      }
       return res.status(200).json({ message: 'Logged in successfully' });
     });
   })(req, res, next);
--- libs/passport.js
+++ libs/passport.js
@@ -1,10 +1,11 @@
 import passport from 'passport';
+import LocalStrategy from 'passport-local';
 import User from '@/models/user';
 import connectMongoDB from '@/libs/mongodb';
 
 connectMongoDB();
 
-passport.use(User.createStrategy());
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
