c274004dff9892a22e8cb390d558ec2c2ed5da35
jaypeter19 <joaopedrofava19@gmail.com>
2024-08-07 16:14:37 -0300

    packages added

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -14,6 +14,7 @@ const Login = () => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
         });
+        
         const data = await response.json();
         if (response.ok) {
             router.push("/"); // Redireciona para a página inicial após login bem-sucedido
