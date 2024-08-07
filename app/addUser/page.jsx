a46c9b5a90bd9d16f06ec280eb840b83f5779d09
jaypeter19 <joaopedrofava19@gmail.com>
2024-08-07 13:07:25 -0300

    views Login e Registrar user

---
+++ app/addUser/page.jsx
@@ -0,0 +1,69 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const AddUser = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const newUser = {
+                username: username,
+                email: email,
+                password: password,
+            };
+            const response = await fetch('/api/auth/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newUser),
+            });
+            if (response.ok) {
+                router.push("/");
+                router.refresh(); //dar um refresh para show up o tópico criado
+            } else {
+                throw new Error("Failed to create new user");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 bg-neutral-950 px-4 py-2 rounded-lg text-neutral-200">
+            <input
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                type="text"
+                placeholder="Usuário"
+                className="p-2 rounded-md"
+                required
+            />
+            <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Email"
+                className="p-2 rounded-md"
+                required
+            />
+            <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Senha"
+                className="p-2 rounded-md"
+                required
+            />
+            <button type="submit" className="hover:bg-neutral-800 font-bold text-white min-w-max">
+                Registrar
+            </button>
+        </form>
+    );
+};
+
+export default AddUser;
---
+++ app/viewLogin/page.jsx
@@ -0,0 +1,50 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch('/api/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        if (response.ok) {
+            router.push("/"); // Redireciona para a página inicial após login bem-sucedido
+        } else {
+            console.error('Login failed:', data);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 bg-neutral-950 px-4 py-2 rounded-lg text-neutral-200">
+            <input
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                type="text"
+                placeholder="Usuário"
+                className="p-2 rounded-md"
+                required
+            />
+            <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Senha"
+                className="p-2 rounded-md"
+                required
+            />
+            <button type="submit" className="hover:bg-neutral-800 font-bold text-white min-w-max">
+                Login
+            </button>
+        </form>
+    );
+};
+
+export default Login;
