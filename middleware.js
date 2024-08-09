39903e09e01ffec8d799b52e80d50d3f03e07164
jaypeter19 <joaopedrofava19@gmail.com>
2024-08-09 14:44:50 -0300

    logout button added to navbar

--- middleware.js
+++ middleware.js
@@ -13,5 +13,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-    matcher: ['/home'], // Adicione suas rotas protegidas aqui
+    matcher: ['/home', '/addTopic', '/addVacation','/editTopic', '/viewEmployee', '/giveFeedback', '/hrPolicy', '/searchEmployee'],
 };
