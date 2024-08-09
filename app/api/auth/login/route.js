3be2fc913d3bda51cadb0c2c3ea5c3dbb5c4b527
jaypeter19 <joaopedrofava19@gmail.com>
2024-08-08 21:21:23 -0300

    cookie session logic added

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -2,6 +2,7 @@
 import connectMongoDB from '@/libs/mongodb';
 import User from '@/models/user';
 import { NextResponse } from 'next/server';
+import cookie from 'cookie';
 
 export async function POST(req) {
     await connectMongoDB();
@@ -17,15 +18,17 @@ export async function POST(req) {
         if (!isPasswordValid) {
             return NextResponse.json({ message: 'Incorrect password.' }, { status: 401 });
         }
-        res.setHeader('Set-Cookie', cookie.serialize('session', token, {
+        const token = user.id;
+        const response = NextResponse.json({ message: 'Login successful' }, { status: 200 });
+        response.headers.set('Set-Cookie', cookie.serialize('session', token, {
             httpOnly: true,
-            secure: 'secret',
+            secure: process.env.NODE_ENV !== 'development',
             maxAge: 60 * 60 * 24, // 1 dia
             sameSite: 'strict',
             path: '/'
         }));
 
-        return NextResponse.json({ message: 'Login successful' }, { status: 200 });
+        return response;
     } catch (error) {
         return NextResponse.json({ message: 'Authentication error', error: error.message }, { status: 500 });
     }
