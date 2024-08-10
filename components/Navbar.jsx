c0fc45331dab24cf3be529ce24d57579290d4bc2
jaypeter19 <joaopedrofava19@gmail.com>
2024-08-10 10:49:38 -0300

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,11 +16,7 @@ export default function Navbar() {
     
     return (
         <nav className="flex justify-between items-center px-8 py-3 rounded-lg">
-<<<<<<< HEAD
-            <Link className="text-neutral-100 font-bold tracking-widest" href={"/home"}> 
-=======
             <Link className="text-neutral-100 font-bold tracking-widest" href={"/home"}>
->>>>>>> d3e8d23bdd2b831a7f133c5c22ac912a732dc599
                 HUMANLINK
             </Link>
             {sessionCookie ? <LogoutButton /> : ''}
