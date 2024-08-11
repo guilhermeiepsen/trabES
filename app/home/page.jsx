c8bcb8a7c23ecbd5c03dcac193c404b399dc5c48
lucas4c <lucas4c@hotmail.com>
2024-08-11 15:18:13 -0300

    mudança do layout da home

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -4,20 +4,20 @@ import { cookies } from 'next/headers';
 // usando o metodo de cookies do next tu tem acesso ao ao id do usuario
 
 export default function Home() {
-  const {value} = cookies().get('user')
-  return <div className="*:p-4 *:bg-neutral-950 my-3 flex flex-col justify-between items-center gap-9 items-start *:rounded-lg  text-neutral-100 tracking-wide">
-          <Link className="text-neutral-100" href={"/employeesList"}>
+  const {idLogado} = cookies().get('user')
+  return <div className="*:flex *:items-end *:justify-end *:p-4 *:bg-neutral-950 *:rounded-lg *:h-52 *:w-full grid grid-cols-2 gap-4 *:text-neutral-100 tracking-wide">
+          <Link className="" href={`/viewEmployee/${idLogado}`}>
+                      MEU PERFIL
+          </Link>
+          <Link className="" href={"/employeesList"}>
                       LISTA DE FUNCIONÁRIOS
           </Link>
-          <Link className="text-neutral-100" href={"/vacationsList"}>
+          <Link className="" href={"/vacationsList"}>
                       LISTA DE FÉRIAS
           </Link>
-          <Link className="text-neutral-100" href={"/hrPolicy"}>
+          <Link className="" href={"/hrPolicy"}>
                       POLÍTICA DE RH
           </Link>
-          <Link className="text-neutral-100" href={"/searchEmployee"}>
-                      PESQUISAR FUNCIONÁRIO
-          </Link>
-          {console.log(value)} 
+          {console.log(idLogado)} 
         </div>
 }
