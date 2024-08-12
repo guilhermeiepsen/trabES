ea8cc8525561c479cb3f68ac7dfa97bfc8eb2a96
lucas4c <lucas4c@hotmail.com>
2024-08-11 23:37:57 -0300

    permissões implementadas na lista de férias

--- app/vacationsList/page.jsx
+++ app/vacationsList/page.jsx
@@ -78,10 +78,10 @@ export default async function EmployeesList() {
                   <h4 className="text-xs text-neutral-300">{t.message}</h4>
                 </div>
                 <div className="flex gap-2">
-                    {t.status == 0 ? <><ApproveVacation id={t._id} managerId={loggedId} response={1}/> <ApproveVacation id={t._id} managerId={loggedId} response={2}/></> : ''}
-                    {t.status == 1 ? 'Aprovada por ' + t.managerId.username : ''}
-                    {t.status == 2 ? 'Negada por ' + t.managerId.username : ''}
-                    <RemoveBtn id={t._id}/>
+                    {t.status == 0 && role != 2 ? <><ApproveVacation id={t._id} managerId={loggedId} response={1}/> <ApproveVacation id={t._id} managerId={loggedId} response={2}/></> : ''}
+                    {t.status == 1 && role != 2 ? 'Aprovada por ' + t.managerId.username : null}
+                    {t.status == 2 ? 'Negada por ' + t.managerId.username : null}
+                    {role == 0 && t.status == 0 ? <RemoveBtn id={t._id}/> : null}
                 </div>
               </div> : null
             ))}
